06112c9e4a8175fb5d0e7be5ed87e589a480e54c
johan <johan@debian-johan.168.1.10>
2025-08-23 21:51:57 -0500

    Primer commit con .gitignore configurado

---
+++ src/main/java/com/edu/sistemarestaurante/SistemaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.edu.sistemarestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaRestauranteApplication.class, args);
+    }
+
+}

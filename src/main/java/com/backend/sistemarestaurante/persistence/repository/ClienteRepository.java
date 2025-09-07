441f2b03fd2b411eac00e48028cd187a84f5e8e1
johan <johan@debian-johan.168.1.10>
2025-09-06 21:59:02 -0500

    refactor: creaion del repository del cliente y la implementacion del servicio para los metodos create, delete, findAll and findById

---
+++ src/main/java/com/backend/sistemarestaurante/persistence/repository/ClienteRepository.java
@@ -0,0 +1,31 @@
+package com.backend.sistemarestaurante.persistence.repository;
+
+import com.backend.sistemarestaurante.persistence.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repositorio para la entidad {@link Cliente}.
+ * Proporciona métodos para la gestión de clientes en la base de datos.
+ */
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    /**
+     * Verifica si existe un cliente con el correo electrónico dado.
+     *
+     * @param email Correo electrónico a verificar.
+     * @return true si el correo ya está registrado, false en caso contrario.
+     */
+    boolean existsByEmail(String email);
+
+    /**
+     * Busca un usuario por su correo electrónico.
+     *
+     * @param email Correo electrónico a buscar.
+     * @return Un {@link Optional} que contiene el usuario si se encuentra, o vacío si no existe.
+     */
+    Optional<Cliente> findByEmail(String email);
+}

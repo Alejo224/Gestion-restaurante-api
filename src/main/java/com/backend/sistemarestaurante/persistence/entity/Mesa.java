407fd64e46aaba7ff4dc905f93fd389affddc6b6
johan <johan@debian-johan.168.1.10>
2025-09-09 17:07:56 -0500

    refactor: Implementacion de los atributos de la entidad Mesa

--- src/main/java/com/backend/sistemarestaurante/persistence/entity/Mesa.java
+++ src/main/java/com/backend/sistemarestaurante/persistence/entity/Mesa.java
@@ -1,12 +1,26 @@
 package com.backend.sistemarestaurante.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+/**
+ * Entidad que representa la tabla en la base de datos.
+ * <p>
+ * La tabla asociada en la base de datos se llama "Mesa" y almacena la información
+ * de cada mesa registrado en la aplicación.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "Mesa")
 public class Mesa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    
+    private String descripcion;
+    
+    private boolean estado; // true = disponible, false = ocupada
 }
